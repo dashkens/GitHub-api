f052cecb31506fabb215f377c6be75827bc9aa56
dashkens <darjajek@gmail.com>
2024-02-10 21:15:15 +0200

    configuring react, tailwind and daisyui

---
+++ github-api/src/App.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+function App() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+        </h1>
+        <div className="bg-purple-500">
+          <h1 className="text-xl">hello2</h1>
+          <button className="btn">Click</button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
