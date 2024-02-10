8fe62f612ac7a215525f93cb1be47538b9bd3f3e
dashkens <darjajek@gmail.com>
2024-02-10 22:02:29 +0200

    created nav and footer

--- github-api/src/App.js
+++ github-api/src/App.js
@@ -1,16 +1,19 @@
 import './index.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-        </h1>
-        <div className="bg-purple-500">
-          <h1 className="text-xl">hello2</h1>
-          <button className="btn">Click</button>
-        </div>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar/>
+        <main className='container mx-auto px-3 pb-12'>
+          Content
+        </main>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
